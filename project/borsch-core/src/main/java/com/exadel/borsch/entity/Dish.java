edc6ecd5558ec0593eb1749daa7ee250891a86c9
Alina <alina_zabavskaya@mail.ru>
2013-07-19 17:47:39 +0300

    entity

---
+++ project/borsch-core/src/main/java/com/exadel/borsch/entity/Dish.java
@@ -0,0 +1,49 @@
+package com.exadel.borsch.entity;
+
+public class Dish {
+    private Integer id;
+    private String name;
+    private Integer price;
+    private String info;
+    private String img;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
